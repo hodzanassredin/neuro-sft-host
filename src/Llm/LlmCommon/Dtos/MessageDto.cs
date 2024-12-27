84ee55e7e5bed4a00ac357871bf9051bf6b788d3
hodzanassredin <shelinanton@gmail.com>
2024-12-27 19:10:17 +0300

    fixed for chat view

--- src/Llm/LlmCommon/Dtos/MessageDto.cs
+++ src/Llm/LlmCommon/Dtos/MessageDto.cs
@@ -5,5 +5,6 @@
         public string Text { get; set; }
         public Ids.Id Id { get; set; }
         public User User { get; set; }
+        public DateTimeOffset CreatedAt { get; set; } = DateTimeOffset.UtcNow;
     }
 }
---
+++ src/Llm/LlmFrontend/Infrastructure/UserHelper.cs
@@ -0,0 +1,30 @@
+﻿using LlmCommon;
+using System.Security.Claims;
+
+namespace LlmFrontend.Infrastructure
+{
+    public static class UserHelper
+    {
+        public static LlmCommon.Dtos.User ToUser(this ClaimsPrincipal? user)
+        {
+            if (user != null && user.Identity != null && user.Identity.IsAuthenticated)
+            {
+
+                var idStr = user.FindFirst(ClaimTypes.NameIdentifier)?.Value;
+                var id = idStr == null ? Ids.Empty : Ids.Parse(idStr);
+                return new LlmCommon.Dtos.User(
+                    id,
+                    user.FindFirst(ClaimTypes.Name)?.Value ?? ""
+                    //user.FindFirstValue(ClaimTypes.Email) ?? "",
+                    //user.Claims
+                    //.Where(c => c.Type == ClaimTypes.Role)
+                    //.Any(c => c.Value == "Admin")
+                );
+            }
+            else
+            {
+                return LlmCommon.Dtos.User.Empty;
+            }
+        }
+    }
+}
