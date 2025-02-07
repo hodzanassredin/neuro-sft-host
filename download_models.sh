2d88a2cf9ef0214f1704d52fe4c9d22f17a37b8b
hodzanassredin <shelinanton@gmail.com>
2025-02-07 15:05:24 +0300

    fixed download script

--- download_models.sh
+++ download_models.sh
@@ -1,5 +1,5 @@
 
 python3 -m venv .hg_download
 source .hg_download/bin/activate
-pip install "huggingface_hub[hf_transfer]"
+python3 -m pip install "huggingface_hub[hf_transfer]"
 HF_HUB_ENABLE_HF_TRANSFER=1 huggingface-cli download hodza/BlackBox-Coder-3B-GGUF --include "BlackBox-Coder-3B-Q8.gguf" --local-dir ./models
