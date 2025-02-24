e14bfc14fb34fa1ee8823805eaf8c80f5dd90d9b
Anton Shelin <shelinanton@gmail.com>
2025-02-24 17:00:51 +0300

    fixed run local

--- 3_run_local.sh
+++ 3_run_local.sh
@@ -1,4 +1,5 @@
 cmake --build . --target run_local
 echo llm server http://localhost:8080/
-echo backend http://localhost:5001/
-echo frontend http://localhost:5002/
+echo backend https://localhost:5001/
+echo frontend https://localhost:5002/
+echo WARNING you probably need to go to front and back and accept self signed certs
